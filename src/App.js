9f756b45c852ab7c916d2a83ffa9fe745414edf3
mbeniac <mandybeniac@gmail.com>
2022-06-10 14:50:38 -0500

    added github url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather - App</div>;
+  return (
+    <div className="App">
+      <h1>Weather - App</h1>
+      <p>
+        This project was created by Mandy Beniac and it is
+        <a
+          href="https://github.com/mbeniac/react-weather-app-Mandy"
+          target="_block"
+        >
+          {" "}
+          open-sourced on GitHub{" "}
+        </a>
+      </p>
+    </div>
+  );
 }
