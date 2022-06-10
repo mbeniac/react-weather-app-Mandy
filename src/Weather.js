c17aee680abada08c2c94cacd0a4dca7590520e7
mbeniac <mandybeniac@gmail.com>
2022-06-10 16:36:39 -0500

    add title and border

---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 9:00pm</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+    </div>
+  );
+}
