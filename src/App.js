0dcb0287023e73bf34669125e9308f7e2bd0b189
mbeniac <mandybeniac@gmail.com>
2022-06-10 16:07:00 -0500

    testing bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather - App</h1>
-      <p>
-        This project was created by Mandy Beniac and it is
-        <a
-          href="https://github.com/mbeniac/react-weather-app-Mandy"
-          target="_block"
-        >
-          {" "}
-          open-sourced on GitHub{" "}
-        </a>
-      </p>
+      <div className="container">
+        <h1>Weather - App</h1>
+
+        <p>
+          This project was created by Mandy Beniac and it is
+          <a
+            href="https://github.com/mbeniac/react-weather-app-Mandy"
+            target="_block"
+          >
+            {" "}
+            open-sourced on GitHub{" "}
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
