8978ed61a42eb1caf32120a1abc78e24cc162191
mbeniac <mandybeniac@gmail.com>
2022-06-10 17:07:47 -0500

    search bar

--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,18 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="col-9">
+          <input
+            type="search"
+            placeholder="Enter a city..."
+            className="form-control"
+          />
+        </div>
+        <div className="col-3">
+          <input type="submit" value="search" className="btn btn-primary" />
+        </div>
+      </form>
       <h1>New York</h1>
       <ul>
         <li>Wednesday 9:00pm</li>
