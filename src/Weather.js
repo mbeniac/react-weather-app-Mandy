c4d1948a85bebcb07db4851c3a9dbd7df97dc847
mbeniac <mandybeniac@gmail.com>
2022-06-12 19:50:16 -0500

    weather css

--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,28 @@
 import React from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
+  const apiKey = "c3be0ab72af9c5b189f134b093f111bc";
   return (
     <div className="Weather">
       <form>
-        <div className="col-9">
-          <input
-            type="search"
-            placeholder="Enter a city..."
-            className="form-control"
-          />
-        </div>
-        <div className="col-3">
-          <input type="submit" value="search" className="btn btn-primary" />
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
+          </div>
         </div>
       </form>
       <h1>New York</h1>
@@ -21,13 +30,15 @@ export default function Weather() {
         <li>Wednesday 9:00pm</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Mostly Cloudy"
           />
-          6℃
+
+          <span className="temperature">6</span>
+          <span className="unit">℃ </span>
         </div>
         <div className="col-6">
           <ul>
