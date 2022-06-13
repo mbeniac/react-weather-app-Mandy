51626a8c47e570019d74bb0f1ba6fedc71610a74
mbeniac <mandybeniac@gmail.com>
2022-06-13 11:53:02 -0500

    weather icon working

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ export default function Weather(props) {
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
 
