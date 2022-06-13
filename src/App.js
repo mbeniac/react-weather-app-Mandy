7fce994286a10a71f29deda706d234bc6cc1a56e
mbeniac <mandybeniac@gmail.com>
2022-06-12 21:26:26 -0500

    default city

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Chicago" />
         <p>
           This project was created by Mandy Beniac and it is
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
     console.log(response.data);
@@ -66,7 +66,7 @@ export default function Weather() {
   } else {
     const apiKey = "c3be0ab72af9c5b189f134b093f111bc";
     let city = "Chicago";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
