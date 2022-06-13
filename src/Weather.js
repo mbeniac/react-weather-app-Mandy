73a2a554c2aa4153da6e4df22097efd4fd3a4e52
mbeniac <mandybeniac@gmail.com>
2022-06-13 13:06:59 -0500

    unneeded file FormattedDate

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
-import FormattedDate from "./FormattedDate";
+
 import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
