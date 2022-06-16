025c4edf9852a6a3e8d6b0696cdf7b89b8c96261
mbeniac <mandybeniac@gmail.com>
2022-06-16 11:57:23 -0500

    css forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,16 +1,19 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
-          <div className="WeatherForecastDay">Thur</div>
-          <WeatherIcon code="01d" />
-          <div className="WeatherForecastTemperatures">
-            <span className="WeatherForecastTemperatureMax">12 </span>
-            <span className="WeatherForecastTemperatureMin">9</span>
+          <div className="WeatherForecast-day">Thur</div>
+
+          <WeatherIcon code="01d" size={32} />
+
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperatures-max">12 </span>
+            <span className="WeatherForecast-temperatures-min">9</span>
           </div>
         </div>
       </div>
